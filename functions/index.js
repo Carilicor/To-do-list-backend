3bad48b4271f8d00b611f870beb67abef0a2a640
Cari Licor <Carilicor@gmail.com>
2022-12-05 15:25:58 -0500

    added security/middleware

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 import functions from 'firebase-functions';
 import express from 'express';
 import cors from 'cors';
+import { userLogin } from './src/user.js'
+import { isUserReallyUser } from './src/middleware.js';
 
 import { getAllList } from './src/toGet.js';
 import { createList } from './src/toPost.js';
@@ -12,8 +14,9 @@ app.use(express.json()) // To enable express
 app.use(cors()) // To enable cors
 
 app.get('/tasks', getAllList) // To read/get to my ToDo list
+app.post('/login', userLogin)
 app.post('/tasks', createList) // To post/add to my ToDo list
-app.patch('/tasks/:docId', updateList) // To update/change to my ToDo list
+app.patch('/tasks/:docId', isUserReallyUser, updateList) // To update/change to my ToDo list
 
 export const api = functions.https.onRequest(app)
 
