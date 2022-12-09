989e1a8a258d9f01eeb5c8f23efd7f5eb2390517
Cari Licor <Carilicor@gmail.com>
2022-12-09 12:20:16 -0500

    finalized project

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import functions from 'firebase-functions';
 import express from 'express';
 import cors from 'cors';
-import { userLogin } from './src/user.js'
+import { userLogin, addNewUser, updateUsers } from './src/user.js'
 import { isUserReallyUser } from './src/middleware.js';
 
 import { getAllList } from './src/toGet.js';
@@ -16,6 +16,9 @@ app.use(cors()) // To enable cors
 app.get('/tasks', getAllList) // To read/get to my ToDo list
 app.post('/login', userLogin)
 app.post('/tasks', createList) // To post/add to my ToDo list
+app.post('/users', addNewUser)
+app.patch('/users/:uid', isUserReallyUser, updateUsers)
+
 // app.delete('/tasks/:docId', isUserReallyUser, deleteItem) // To delete item from my ToDo list
 
 export const api = functions.https.onRequest(app) 
